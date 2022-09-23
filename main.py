866515843dec1096d469d60cf460faec71498e64
juvieespinosa <jhuv89@gmail.com>
2022-09-23 07:28:10 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, redirect, url_for, request, flash, send_file
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
+from dotenv import load_dotenv
+
+import smtplib
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+    return render_template('index.html')
+
+if __name__ =="__main__":
+    app.run(debug=True)
